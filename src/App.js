5681f5eb6c9f0b191db6f59ad9d63f9f5b2da60d
mzq737 <60275847+mzq737@users.noreply.github.com>
2020-08-29 00:06:58 -0400

    bug0fixed

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://quiet-savannah-29855.herokuapp.com', {
+    fetch('https://quiet-savannah-29855.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -94,7 +94,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://quiet-savannah-29855.herokuapp.com', {
+          fetch('https://quiet-savannah-29855.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
